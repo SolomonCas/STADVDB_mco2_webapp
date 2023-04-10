2078981627e544c8178482cd5ba0f16217205d6c
SolomonCas <solomon_castillo@dlsu.edu.ph>
2023-04-10 11:43:43 +0800

    new mysql

--- mysql.js
+++ mysql.js
@@ -2,13 +2,16 @@ const dotenv = require('dotenv');
 const mysql = require('mysql2');
 dotenv.config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
-  password: process.env.PASSWORD
+  password: process.env.PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
+connection.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting to MySQL server: ' + err.stack);
     return;
