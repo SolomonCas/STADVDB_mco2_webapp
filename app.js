cc1b0c24f764fd2f193d82e62705a89b64719ad4
Mark Adrian Escobar <69063424+markadriii@users.noreply.github.com>
2023-04-11 15:06:20 +0800

    added update function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mysqlConnection = require('./mysql');
 const exphbs = require('express-handlebars');
 const hostname = '172.16.3.128'; // for node2
@@ -10,9 +11,10 @@ app.engine("hbs", exphbs.engine({extname: 'hbs', partialsDir: __dirname + '/view
 app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  mysqlConnection.query('SELECT * FROM movies WHERE id < 10', (err, rows, fields) => {
+  mysqlConnection.query('SELECT * FROM movies WHERE id LIMIT 10', (err, rows, fields) => {
     if (err) {
       console.error('Error querying MySQL database: ' + err.stack);
       res.status(500).send('Error querying MySQL database');
@@ -22,6 +24,23 @@ app.get('/', (req, res) => {
         movies:rows
     })
   });
+});
+
+app.post('/update', async (req, res) => {
+
+    const movie = req.body;
+    const statement = 'UPDATE movies SET name=?, year=?, `rank`=?, genre=?, director_first_name=?, director_last_name=? WHERE id=?';
+    const values = [movie.update_name, movie.update_year, movie.update_rank, movie.update_genre, movie.update_director_first_name, movie.update_director_last_name, movie.update_id];
+    
+    mysqlConnection.query(statement, values, (err, rows, fields) => {
+      if (err) {
+        console.error('Error querying MySQL database: ' + err.stack);
+        res.status(500).send('Error querying MySQL database');
+        return;
+      }
+      console.log('Successfully Updated Data');
+      res.redirect('/');
+    });
 });
 
 app.listen(80, () => {
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
     console.log("Document Ready");
     $('#movies_table').DataTable();
+
+    $('.selected_row').on('click', (function() {
+      const parentTr = $(this).closest('tr');
+      const selected_row = {
+        id: parentTr.find('td').eq(1).text(),
+        name: parentTr.find('td').eq(2).text(),
+        year: parentTr.find('td').eq(3).text(),
+        rank: parentTr.find('td').eq(4).text(),
+        genre: parentTr.find('td').eq(5).text(),
+        director_first_name: parentTr.find('td').eq(6).text(),
+        director_last_name: parentTr.find('td').eq(7).text()
+      }
+
+      $('#update_id').val(selected_row.id);
+      $('#update_name').val(selected_row.name);
+      $('#update_year').val(selected_row.year);
+      $('#update_rank').val(selected_row.rank);
+      $('#update_genre').val(selected_row.genre);
+      $('#update_director_first_name').val(selected_row.director_first_name);
+      $('#update_director_last_name').val(selected_row.director_last_name);
+
+    }));
+
 });
