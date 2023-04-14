789e705baad57bc45f0cf6c1d82c123357fe9da7
SolomonCas <solomon_castillo@dlsu.edu.ph>
2023-04-15 00:41:40 +0800

    node 1 views

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysqlConnection = require('./mysql');
+const {node_1, node_2, node_3} = require('./mysql');
 const exphbs = require('express-handlebars');
 
 const app = express();
@@ -14,6 +14,9 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+
+
 app.get('/', (req, res) => {
 	res.redirect("/1");
 });
@@ -26,194 +29,254 @@ app.get('/:page', (req, res) => {
 		const recordsPerPage = 10; // Set the number of records per page
 	
 		// Retrieve the total number of records in the database table
-		mysqlConnection.query('SELECT COUNT(*) AS total FROM movies', (err, result) => {
-			if (err) throw err;
-			const totalRecords = result[0].total;
-	
-			// Calculate the total number of pages
-			const totalPages = Math.ceil(totalRecords / recordsPerPage);
-	
-			// Calculate the offset for the SQL query based on the current page
-			const offset = (page - 1) * recordsPerPage;
-			// Retrieve the data for the current page
-			mysqlConnection.query('SELECT * FROM movies LIMIT ? OFFSET ?', [recordsPerPage, offset], (err, results) => {
-				if (err) throw err;
-				res.render('index', { movies: results, current_page: page, total_pages: totalPages});
-			});
-		});
-	}
-	
-});
-
-app.get('/filter/:page', (req, res) => {
-	const page = parseInt(req.params.page);
-    const recordsPerPage = 10; // Set the number of records per page
-    // Retrieve the total number of records in the database table
-    mysqlConnection.query(`SELECT COUNT(*) AS total FROM movies WHERE ${filter_attribute} ${filter_operator} ${filter_input}`, (err, result) => {
-        if (err) throw err;
-        const totalRecords = result[0].total;
-
-        // Calculate the total number of pages
-        const totalPages = Math.ceil(totalRecords / recordsPerPage);
-
-        // Calculate the offset for the SQL query based on the current page
-        const offset = (page - 1) * recordsPerPage;
-
-        // Retrieve the data for the current page
-        mysqlConnection.query(`SELECT * FROM movies WHERE ${filter_attribute} ${filter_operator} ${filter_input} LIMIT ? OFFSET ?`, [recordsPerPage, offset], (err, results) => {
-            if (err) throw err;
-            res.render('index', { movies: results, current_page: page, total_pages: totalPages});
-        });
-    });
-});
-
-
-app.post('/filter/:page', (req, res) => {
-	const filter = req.body;
-	const page = parseInt(req.params.page);
-    const recordsPerPage = 10; // Set the number of records per page
-	filter_operator = filter.operator;
-	filter_attribute = filter.attribute;
-	filter_input = filter.filter_input;
-    // Retrieve the total number of records in the database table
-    mysqlConnection.query(`SELECT COUNT(*) AS total FROM movies WHERE ${filter.attribute} ${filter.operator} ${filter.filter_input}`, (err, result) => {
-        if (err) throw err;
-        const totalRecords = result[0].total;
-
-        // Calculate the total number of pages
-        const totalPages = Math.ceil(totalRecords / recordsPerPage);
-
-        // Calculate the offset for the SQL query based on the current page
-        const offset = (page - 1) * recordsPerPage;
-
-        // Retrieve the data for the current page
-        mysqlConnection.query(`SELECT * FROM movies WHERE ${filter.attribute} ${filter.operator} ${filter.filter_input} LIMIT ? OFFSET ?`, [recordsPerPage, offset], (err, results) => {
-            if (err) throw err;
-            res.render('index', { movies: results, current_page: page, total_pages: totalPages});
-        });
-    });
-});
-
-app.get('/search/:page', (req, res) => {
-	const page = parseInt(req.params.page);
-    const recordsPerPage = 10; // Set the number of records per page
-    // Retrieve the total number of records in the database table
-    mysqlConnection.query(`SELECT COUNT(*) AS total FROM movies WHERE ${search_attribute} LIKE "%${search_input}%" ORDER BY ${search_attribute}`, (err, result) => {
-        if (err) throw err;
-        const totalRecords = result[0].total;
-
-        // Calculate the total number of pages
-        const totalPages = Math.ceil(totalRecords / recordsPerPage);
-
-        // Calculate the offset for the SQL query based on the current page
-        const offset = (page - 1) * recordsPerPage;
-
-        // Retrieve the data for the current page
-        mysqlConnection.query(`SELECT * FROM movies WHERE ${search_attribute} LIKE "%${search_input}%" ORDER BY ${search_attribute} LIMIT ? OFFSET ?`, [recordsPerPage, offset], (err, results) => {
-            if (err) throw err;
-            res.render('index', { movies: results, current_page: page, total_pages: totalPages});
-        });
-    });
-});
-
-app.post('/search/:page', (req, res) => {
-	const search = req.body;
-	const page = parseInt(req.params.page);
-    const recordsPerPage = 10; // Set the number of records per page
-	search_attribute = search.attribute;
-	search_input = search.search_input;
-    // Retrieve the total number of records in the database table
-    mysqlConnection.query(`SELECT COUNT(*) AS total FROM movies WHERE ${search.attribute} LIKE "%${search.search_input}%" ORDER BY ${search.attribute}`, (err, result) => {
-        if (err) throw err;
-        const totalRecords = result[0].total;
-
-        // Calculate the total number of pages
-        const totalPages = Math.ceil(totalRecords / recordsPerPage);
-
-        // Calculate the offset for the SQL query based on the current page
-        const offset = (page - 1) * recordsPerPage;
-
-        // Retrieve the data for the current page
-        mysqlConnection.query(`SELECT * FROM movies WHERE ${search.attribute} LIKE "%${search.search_input}%" ORDER BY ${search.attribute} LIMIT ? OFFSET ?`, [recordsPerPage, offset], (err, results) => {
-            if (err) throw err;
-            res.render('index', { movies: results, current_page: page, total_pages: totalPages});
-        });
-    });
-});
-
-
-app.post('/insert', (req, res) => {
-	const movie = req.body;
-
-	mysqlConnection.query(`SELECT max(id) + 1 AS new FROM movies`, (err, resp) => {
-		if (err) {
-			console.error('Error querying MySQL database: ' + err.stack);
-			res.status(500).send('Error querying MySQL database');
-			return;
-		}
-		else {
-			new_id = resp[0].new;
-			console.log("new_id: " + new_id);
-			
-			const values = [new_id, movie.name, movie.year, movie.rank, movie.genre, movie.director_first_name, movie.director_last_name];
-			const statement = `INSERT INTO movies (id, name, year, \`rank\`, genre, director_first_name, director_last_name) VALUES (?, ?, ?, COALESCE(NULLIF(?, ''), NULL), COALESCE(NULLIF(?, ''), NULL), COALESCE(NULLIF(?, ''), NULL), COALESCE(NULLIF(?, ''), NULL))`;
-			
-			mysqlConnection.query(statement, values, (err, results) => {
-				if (err) {
-					console.error('Error querying MySQL database: ' + err.stack);
-					res.status(500).send('Error querying MySQL database');
-					return;
-				}
-				else {
-					console.log("Successfully Inserted Data");
-					res.redirect('/');
-				}
-			})
-		}
-	});
-	
-});
-
-app.post('/update', async (req, res) => {
-
-	const movie = req.body;
-	const statement = `UPDATE movies SET name = COALESCE(NULLIF(?, ''), name), year = COALESCE(NULLIF(?, ''), year), \`rank\` = COALESCE(NULLIF(?, ''), \`rank\`), genre = COALESCE(NULLIF(?, ''), genre), director_first_name = COALESCE(NULLIF(?, ''), director_first_name), director_last_name = COALESCE(NULLIF(?, ''), director_last_name) WHERE id = ?`;
-	if(movie.update_id == ''){
-		res.redirect('/');
-	}
-	else{
-		const values = [movie.update_name, movie.update_year, movie.update_rank, movie.update_genre, movie.update_director_first_name, movie.update_director_last_name, movie.update_id];
-
-		mysqlConnection.query(statement, values, (err, rows, fields) => {
-			if (err) {
-				console.error('Error querying MySQL database: ' + err.stack);
-				res.status(500).send('Error querying MySQL database');
-				return;
+		node_1.query('SELECT COUNT(*) AS total FROM movies', (err, result) => {
+			if (err){
+				node_2.query('SELECT COUNT(*) AS total FROM movies', (err, result) => {
+					if (err) throw err;
+					var totalRecords = result[0].total;
+					// Calculate the total number of pages
+					var totalPages = Math.ceil(totalRecords / recordsPerPage);
+					var node_2_total_pages = totalPages;
+					// Retrieve the data for the current page
+					node_3.query('SELECT COUNT(*) AS total FROM movies', (err, data) => {
+						if (err) throw err;
+						totalRecords += data[0].total;
+					
+						// Calculate the total number of pages
+						const totalPages = Math.ceil(totalRecords / recordsPerPage);
+					
+						// Calculate the offset for the SQL query based on the current page
+						var offset = (page - 1) * recordsPerPage;
+						console.log("node_2_total_pages: " + node_2_total_pages);
+						// Retrieve the data for the current page
+						if(node_2_total_pages >= page){
+							console.log("node_2");
+							node_2.query('SELECT * FROM movies LIMIT ? OFFSET ?', [recordsPerPage, offset], (err, results) => {
+								if (err) throw err;
+								res.render('index', { movies: results, current_page: page, total_pages: totalPages});
+							});
+						}
+						else{
+							console.log("node_3");
+							offset = (page - 1 - node_2_total_pages) * recordsPerPage;
+							node_3.query('SELECT * FROM movies LIMIT ? OFFSET ?', [recordsPerPage, offset], (err, results) => {
+								if (err) throw err;
+								res.render('index', { movies: results, current_page: page, total_pages: totalPages});
+							});
+						}
+						
+					});
+					
+				});
 			}
-			console.log('Successfully Updated Data');
-			res.redirect('/');
-		});
+			else{
+				const totalRecords = result[0].total;
+			
+				// Calculate the total number of pages
+				const totalPages = Math.ceil(totalRecords / recordsPerPage);
+				
+				// Calculate the offset for the SQL query based on the current page
+				const offset = (page - 1) * recordsPerPage;
+				// Retrieve the data for the current page
+				node_1.query('SELECT * FROM movies LIMIT ? OFFSET ?', [recordsPerPage, offset], (err, results) => {
+					if (err) throw err;
+					res.render('index', { movies: results, current_page: page, total_pages: totalPages});
+				});
+			}
+			
+		});	
+		
 	}
 	
 });
 
-app.post('/delete', (req, res) => {
+// app.get('/filter/:page', (req, res) => {
+// 	const page = parseInt(req.params.page);
+//     const recordsPerPage = 10; // Set the number of records per page
+//     // Retrieve the total number of records in the database table'
+// 	// getConnection(function(connection){
+// 	// 	connection.query(`SELECT COUNT(*) AS total FROM movies WHERE ${filter_attribute} ${filter_operator} ${filter_input}`, (err, result) => {
+// 	// 		if (err) throw err;
+// 	// 		const totalRecords = result[0].total;
+	
+// 	// 		// Calculate the total number of pages
+// 	// 		const totalPages = Math.ceil(totalRecords / recordsPerPage);
+	
+// 	// 		// Calculate the offset for the SQL query based on the current page
+// 	// 		const offset = (page - 1) * recordsPerPage;
+	
+// 	// 		// Retrieve the data for the current page
+// 	// 		connection.query(`SELECT * FROM movies WHERE ${filter_attribute} ${filter_operator} ${filter_input} LIMIT ? OFFSET ?`, [recordsPerPage, offset], (err, results) => {
+// 	// 			if (err) throw err;
+// 	// 			res.render('index', { movies: results, current_page: page, total_pages: totalPages});
+// 	// 		});
+// 	// 	});
+// 	// });
+    
+// });
 
-	const id = req.body.selectedrow;
-	console.log(id);
+
+// app.post('/filter/:page', (req, res) => {
+// 	const filter = req.body;
+// 	const page = parseInt(req.params.page);
+//     const recordsPerPage = 10; // Set the number of records per page
+// 	filter_operator = filter.operator;
+// 	filter_attribute = filter.attribute;
+// 	filter_input = filter.filter_input;
+//     // Retrieve the total number of records in the database table
+// 	// getConnection(function(conn){
+// 	// 	conn.query(`SELECT COUNT(*) AS total FROM movies WHERE ${filter.attribute} ${filter.operator} ${filter.filter_input}`, (err, result) => {
+// 	// 		if (err) throw err;
+// 	// 		const totalRecords = result[0].total;
+	
+// 	// 		// Calculate the total number of pages
+// 	// 		const totalPages = Math.ceil(totalRecords / recordsPerPage);
+	
+// 	// 		// Calculate the offset for the SQL query based on the current page
+// 	// 		const offset = (page - 1) * recordsPerPage;
+	
+// 	// 		// Retrieve the data for the current page
+// 	// 		conn.query(`SELECT * FROM movies WHERE ${filter.attribute} ${filter.operator} ${filter.filter_input} LIMIT ? OFFSET ?`, [recordsPerPage, offset], (err, results) => {
+// 	// 			if (err) throw err;
+// 	// 			res.render('index', { movies: results, current_page: page, total_pages: totalPages});
+// 	// 		});
+// 	// 	});
+// 	// });
+    
+// });
+
+// app.get('/search/:page', (req, res) => {
+// 	const page = parseInt(req.params.page);
+//     const recordsPerPage = 10; // Set the number of records per page
+//     // Retrieve the total number of records in the database table
+// 	// getConnection(function(conn){
+// 	// 	conn.query(`SELECT COUNT(*) AS total FROM movies WHERE ${search_attribute} LIKE "%${search_input}%" ORDER BY ${search_attribute}`, (err, result) => {
+// 	// 		if (err) throw err;
+// 	// 		const totalRecords = result[0].total;
+	
+// 	// 		// Calculate the total number of pages
+// 	// 		const totalPages = Math.ceil(totalRecords / recordsPerPage);
+	
+// 	// 		// Calculate the offset for the SQL query based on the current page
+// 	// 		const offset = (page - 1) * recordsPerPage;
+	
+// 	// 		// Retrieve the data for the current page
+// 	// 		conn.query(`SELECT * FROM movies WHERE ${search_attribute} LIKE "%${search_input}%" ORDER BY ${search_attribute} LIMIT ? OFFSET ?`, [recordsPerPage, offset], (err, results) => {
+// 	// 			if (err) throw err;
+// 	// 			res.render('index', { movies: results, current_page: page, total_pages: totalPages});
+// 	// 		});
+// 	// 	});
+// 	// });
+    
+// });
+
+// app.post('/search/:page', (req, res) => {
+// 	const search = req.body;
+// 	const page = parseInt(req.params.page);
+//     const recordsPerPage = 10; // Set the number of records per page
+// 	search_attribute = search.attribute;
+// 	search_input = search.search_input;
+//     // Retrieve the total number of records in the database table
+// 	getConnection(function(conn){
+// 		conn.query(`SELECT COUNT(*) AS total FROM movies WHERE ${search.attribute} LIKE "%${search.search_input}%" ORDER BY ${search.attribute}`, (err, result) => {
+// 			if (err) throw err;
+// 			const totalRecords = result[0].total;
+	
+// 			// Calculate the total number of pages
+// 			const totalPages = Math.ceil(totalRecords / recordsPerPage);
+	
+// 			// Calculate the offset for the SQL query based on the current page
+// 			const offset = (page - 1) * recordsPerPage;
+	
+// 			// Retrieve the data for the current page
+// 			conn.query(`SELECT * FROM movies WHERE ${search.attribute} LIKE "%${search.search_input}%" ORDER BY ${search.attribute} LIMIT ? OFFSET ?`, [recordsPerPage, offset], (err, results) => {
+// 				if (err) throw err;
+// 				res.render('index', { movies: results, current_page: page, total_pages: totalPages});
+// 			});
+// 		});
+// 	});
+    
+// });
 
 
-	mysqlConnection.query(`DELETE FROM movies WHERE id = ${id}`, (err, rows, fields) => {
-		if (err) {
-			console.error('Error querying MySQL database: ' + err.stack);
-			res.status(500).send('Error querying MySQL database');
-			return;
-		}
-		console.log('Successful Deletion');
-		res.redirect('/');
-	})
+// app.post('/insert', (req, res) => {
+// 	const movie = req.body;
+// 	getConnection(function(mysqlConnection){
+// 		mysqlConnection.query(`SELECT max(id) + 1 AS new FROM movies`, (err, resp) => {
+// 			if (err) {
+// 				console.error('Error querying MySQL database: ' + err.stack);
+// 				res.status(500).send('Error querying MySQL database');
+// 				return;
+// 			}
+// 			else {
+// 				new_id = resp[0].new;
+// 				console.log("new_id: " + new_id);
+				
+// 				const values = [new_id, movie.name, movie.year, movie.rank, movie.genre, movie.director_first_name, movie.director_last_name];
+// 				const statement = `INSERT INTO movies (id, name, year, \`rank\`, genre, director_first_name, director_last_name) VALUES (?, ?, ?, COALESCE(NULLIF(?, ''), NULL), COALESCE(NULLIF(?, ''), NULL), COALESCE(NULLIF(?, ''), NULL), COALESCE(NULLIF(?, ''), NULL))`;
+				
+// 				mysqlConnection.query(statement, values, (err, results) => {
+// 					if (err) {
+// 						console.error('Error querying MySQL database: ' + err.stack);
+// 						res.status(500).send('Error querying MySQL database');
+// 						return;
+// 					}
+// 					else {
+// 						console.log("Successfully Inserted Data");
+// 						res.redirect('/');
+// 					}
+// 				})
+// 			}
+// 		});
+// 	});
+	
+	
+// });
 
-});
+// app.post('/update', async (req, res) => {
+
+// 	const movie = req.body;
+// 	const statement = `UPDATE movies SET name = COALESCE(NULLIF(?, ''), name), year = COALESCE(NULLIF(?, ''), year), \`rank\` = COALESCE(NULLIF(?, ''), \`rank\`), genre = COALESCE(NULLIF(?, ''), genre), director_first_name = COALESCE(NULLIF(?, ''), director_first_name), director_last_name = COALESCE(NULLIF(?, ''), director_last_name) WHERE id = ?`;
+// 	if(movie.update_id == ''){
+// 		res.redirect('/');
+// 	}
+// 	else{
+// 		const values = [movie.update_name, movie.update_year, movie.update_rank, movie.update_genre, movie.update_director_first_name, movie.update_director_last_name, movie.update_id];
+// 		getConnection(function(mysqlConnection){
+// 			mysqlConnection.query(statement, values, (err, rows, fields) => {
+// 				if (err) {
+// 					console.error('Error querying MySQL database: ' + err.stack);
+// 					res.status(500).send('Error querying MySQL database');
+// 					return;
+// 				}
+// 				console.log('Successfully Updated Data');
+// 				res.redirect('/');
+// 			});
+// 		});
+		
+// 	}
+	
+// });
+
+// app.post('/delete', (req, res) => {
+
+// 	const id = req.body.selectedrow;
+// 	console.log(id);
+
+// 	getConnection(function(mysqlConnection){
+// 		mysqlConnection.query(`DELETE FROM movies WHERE id = ${id}`, (err, rows, fields) => {
+// 			if (err) {
+// 				console.error('Error querying MySQL database: ' + err.stack);
+// 				res.status(500).send('Error querying MySQL database');
+// 				return;
+// 			}
+// 			console.log('Successful Deletion');
+// 			res.redirect('/');
+// 		});
+// 	});
+	
+
+// });
 app.listen(80, () => {
-	console.log('Node.js server running on port 80');
+ 	console.log('Node.js server running on port 80');
 });
--- mysql.js
+++ mysql.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 const mysql = require('mysql2');
 dotenv.config();
 
-const connection = mysql.createPool({
+const node_1 = mysql.createPool({
 	host: process.env.HOST,
 	port: process.env.PORT,
 	user: process.env.USER,
@@ -10,13 +10,51 @@ const connection = mysql.createPool({
 	database: process.env.DATABASE
 });
 
-connection.getConnection((err, connection) => {
-	if (err) {
-		console.error('Error connecting to MySQL server: ' + err.stack);
-		return;
-	}
-
-	console.log('Connected to MySQL server with id ' + connection.threadId);
+const node_2 = mysql.createPool({
+	host: process.env.HOST2,
+	port: process.env.PORT2,
+	user: process.env.USER2,
+	password: process.env.PASSWORD2,
+	database: process.env.DATABASE2
 });
 
-module.exports = connection;
+const node_3 = mysql.createPool({
+	host: process.env.HOST3,
+	port: process.env.PORT3,
+	user: process.env.USER3,
+	password: process.env.PASSWORD3,
+	database: process.env.DATABASE3
+});
+
+node_1.getConnection((err, conn) => {
+	if (err) {
+		console.error('Error connecting to MySQL server: ' + err.stack);
+	}
+	else{
+		console.log('Connected to MySQL server node 1 with id ' + conn.threadId);
+	}
+	
+});
+node_2.getConnection((err, conn) => {
+	if (err) {
+		console.error('Error connecting to MySQL server: ' + err.stack);
+
+	}
+	else{
+		console.log('Connected to MySQL server node 2 with id ' + conn.threadId);
+	}
+
+	
+});
+node_3.getConnection((err, conn) => {
+	if (err) {
+		console.error('Error connecting to MySQL server: ' + err.stack);
+		
+	}
+	else{
+		console.log('Connected to MySQL server node 3 with id ' + conn.threadId);
+	}
+
+});
+
+module.exports = {node_1, node_2, node_3};
